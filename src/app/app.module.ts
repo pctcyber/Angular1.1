7882d036a2aabacaafbc307fcd19e0ef65838646
cong tin <pct.hcmut@gmail.com>
2021-04-10 16:36:16 +0700

    PHÂN TÁCH NHÓM MODULE VÀ COMPONENT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-import { FootterComponent } from './footter/footter.component';
-import { TrangChuComponent } from './trang-chu/trang-chu.component';
+// import { FirstComponent } from './first/first.component';
+// import { HeaderComponent } from './header/header.component';
+// import { ContentComponent } from './content/content.component';
+// import { SideBarComponent } from './side-bar/side-bar.component';
+// import { FootterComponent } from './footter/footter.component';
+// import { TrangChuComponent } from './trang-chu/trang-chu.component';
+import { LayoutModuleModule } from './layout-module/layout-module.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
-    HeaderComponent,
-    ContentComponent,
-    SideBarComponent,
-    FootterComponent,
-    TrangChuComponent,
+    // FirstComponent,
+    // HeaderComponent,
+    // ContentComponent,
+    // SideBarComponent,
+    // FootterComponent,
+    // TrangChuComponent,
+    
    
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
