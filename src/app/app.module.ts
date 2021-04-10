fabfcba366fa93c28cad1ccc695fa4f1e30520dd
cong tin <pct.hcmut@gmail.com>
2021-04-10 15:40:45 +0700

    settup bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { FootterComponent } from './footter/footter.component';
+import { TrangChuComponent } from './trang-chu/trang-chu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    HeaderComponent,
+    ContentComponent,
+    SideBarComponent,
+    FootterComponent,
+    TrangChuComponent,
+   
+    
   ],
   imports: [
     BrowserModule,
