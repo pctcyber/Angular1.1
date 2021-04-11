7d5ec52ea162edfbb3850ea3eddf3f1716d28109
cong tin <pct.hcmut@gmail.com>
2021-04-11 14:11:09 +0700

    binding with object

--- src/app/Module/DataBinding/one-way-binding/one-way-binding.component.ts
+++ src/app/Module/DataBinding/one-way-binding/one-way-binding.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class OneWayBindingComponent implements OnInit {
 
   public name:string = 'Henry MasterYi'
+  public hocVien:any = {hoTen:'Nguyễn Văn A', gioiTinh:'Nam'}
   constructor() { }
 
   ngOnInit(): void {
